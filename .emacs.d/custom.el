75cf120c014c8b88f85cfde2eccaa4ada6d77c72
bluesboy <bluesboychaosnix@gmail.com>
2025-06-05 00:30:25 +0530

    Adding emacs config

---
+++ .emacs.d/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+     default))
+ '(package-selected-packages
+   '(all-the-icons-ivy-rich all-the-icons-nerd-fonts company-box counsel
+			    dashboard doom-modeline doom-themes
+			    highlight-quoted highlight-symbol
+			    modus-themes paredit rainbow-delimiters
+			    sly treemacs-all-the-icons vertico vterm))
+ '(warning-suppress-log-types '((use-package) (native-compiler)))
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

ccd806c3d02aea89e619a512db6e3c23224fb0bb
bluesboy <bluesboychaosnix@gmail.com>
2025-12-02 18:22:07 +0000

    update scheme

--- emacs/.config/emacs/lisp/scheme-dev.el
+++ emacs/.config/emacs/lisp/scheme-dev.el
@@ -1,19 +1,13 @@
-;;; scheme-dev.el --- Scheme Development Config (Guile) -*- lexical-binding: t; -*-
-
-;; ===================================================================
-;; Scheme Editing Essentials
-;; ===================================================================
+;;; scheme-dev.el --- Scheme Development Config (Guile Scheme) -*- lexical-binding: t; -*-
 
 ;; -----------------------------
 ;; Smartparens
 ;; -----------------------------
 (use-package smartparens
-  :ensure t
-  :hook ((scheme-mode
-          emacs-lisp-mode
+  :hook ((emacs-lisp-mode
           lisp-mode
-          slime-repl-mode)
-         . smartparens-mode)
+          slime-repl-mode
+          scheme-mode) . smartparens-mode)
   :config
   (require 'smartparens-config)
   (smartparens-global-mode 1)
@@ -23,67 +17,54 @@
         sp-highlight-pair-overlay nil))
 
 ;; -----------------------------
-;; Rainbow Delimiters
-;; -----------------------------
-(use-package rainbow-delimiters
-  :ensure t
-  :hook (scheme-mode . rainbow-delimiters-mode)
-  :config
-  ;; Doom-inspired manual colors
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (with-eval-after-load 'rainbow-delimiters
-                (set-face-attribute 'rainbow-delimiters-depth-1-face nil :foreground "#FF0000")
-                (set-face-attribute 'rainbow-delimiters-depth-2-face nil :foreground "#FF8C00")
-                (set-face-attribute 'rainbow-delimiters-depth-3-face nil :foreground "#FFFF00")
-                (set-face-attribute 'rainbow-delimiters-depth-4-face nil :foreground "#00FF00")
-                (set-face-attribute 'rainbow-delimiters-depth-5-face nil :foreground "#56B6C2")
-                (set-face-attribute 'rainbow-delimiters-depth-6-face nil :foreground "#9467BD")
-                (set-face-attribute 'rainbow-delimiters-depth-7-face nil :foreground "#D19A66")
-                (set-face-attribute 'rainbow-delimiters-unmatched-face nil
-                                    :foreground "white"
-                                    :background "#FF0000"
-                                    :weight 'bold)))))
-
-;; -----------------------------
-;; Pretty symbols
-;; -----------------------------
-;; (add-hook 'scheme-mode-hook
-;;           (lambda ()
-;;             (setq prettify-symbols-alist
-;;                   '(("lambda" . ?λ)
-;;                     ("->"     . ?→)
-;;                     ("=>"     . ?⇒)
-;;                     ("<="     . ?≤)
-;;                     (">="     . ?≥)))
-;;             (prettify-symbols-mode 1)))
-
-;; -----------------------------
 ;; Eldoc + Hover Docs
 ;; -----------------------------
-(add-hook 'scheme-mode-hook #'eldoc-mode)
-
 (use-package eldoc-box
-  :ensure t
   :hook (scheme-mode . eldoc-box-hover-mode))
 
+(add-hook 'scheme-mode-hook #'eldoc-mode)
+
+;; -----------------------------
+;; Macro Expansion
+;; -----------------------------
 (use-package macrostep
-  :ensure t
-  :defer t)
+  :after scheme
+  :bind (:map scheme-mode-map
+              ("C-c e" . macrostep-expand)))
 
-(with-eval-after-load 'scheme
-  (define-key scheme-mode-map (kbd "C-c e") #'macrostep-expand))
+;; -----------------------------
+;; Rainbow Delimiters (custom colors)
+;; -----------------------------
+(use-package rainbow-delimiters
+  :hook ((prog-mode
+          lisp-mode
+          emacs-lisp-mode
+          scheme-mode
+          scheme-ts-mode
+          sly-mrepl-mode
+          slime-repl-mode) . rainbow-delimiters-mode)
+  :config
+  (add-hook 'scheme-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'scheme-ts-mode-hook #'rainbow-delimiters-mode)
 
-
-;; ===================================================================
-;; Completion & Documentation
-;; ===================================================================
+  (add-hook
+   'emacs-startup-hook
+   (lambda ()
+     (with-eval-after-load 'rainbow-delimiters
+       (set-face-attribute 'rainbow-delimiters-depth-1-face nil :foreground "#FF0000")
+       (set-face-attribute 'rainbow-delimiters-depth-2-face nil :foreground "#FF8C00")
+       (set-face-attribute 'rainbow-delimiters-depth-3-face nil :foreground "#FFFF00")
+       (set-face-attribute 'rainbow-delimiters-depth-4-face nil :foreground "#00FF00")
+       (set-face-attribute 'rainbow-delimiters-depth-5-face nil :foreground "#56B6C2")
+       (set-face-attribute 'rainbow-delimiters-depth-6-face nil :foreground "#9467BD")
+       (set-face-attribute 'rainbow-delimiters-depth-7-face nil :foreground "#D19A66")
+       (set-face-attribute 'rainbow-delimiters-unmatched-face nil
+                           :foreground "white" :background "#FF0000" :weight 'bold)))))
 
 ;; -----------------------------
 ;; Corfu Completion
 ;; -----------------------------
 (use-package corfu
-  :ensure t
   :init
   (global-corfu-mode)
   :custom
@@ -93,116 +74,72 @@
   (corfu-quit-no-match t)
   (corfu-scroll-margin 4)
   :bind (:map corfu-map
-              ("TAB"     . corfu-next)
-              ([tab]     . corfu-next)
-              ("S-TAB"   . corfu-previous)
+              ("TAB" . corfu-next)
+              ([tab] . corfu-next)
+              ("S-TAB" . corfu-previous)
               ([backtab] . corfu-previous)))
 
-;; -----------------------------
-;; Cape Completion Sources
-;; -----------------------------
+;; Cape extensions
 (use-package cape
-  :ensure t
   :init
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-symbol)
   (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
-;; ===================================================================
-;; Guile via Geiser (Active Backend)
-;; ===================================================================
-
-(use-package geiser
-  :ensure t
-  :init
-  (setq geiser-active-implementations '(guile)
-        geiser-default-implementation 'guile
-        geiser-repl-query-on-kill-p nil
-        geiser-repl-use-other-window t
-        geiser-repl-history-filename "~/.config/emacs/geiser-history"))
-
-(use-package geiser-guile
-  :ensure t)
-
 
 ;; -----------------------------
-;; Inline Eval Results
+;; Guile Scheme REPL (Comint)
 ;; -----------------------------
-(use-package eros
-  :ensure t
-  :hook (scheme-mode . eros-mode))
+(defun run-guile ()
+  "Run Guile REPL in a right-side split."
+  (interactive)
+  (unless (comint-check-proc "*guile*")
+    (set-buffer (make-comint "guile" "/usr/bin/guile")))
+  (let ((repl-buffer (get-buffer "*guile*"))
+        (cur-window (selected-window)))
+    (unless (get-buffer-window repl-buffer)
+      (let ((new-window (split-window-right)))
+        (set-window-buffer new-window repl-buffer)))
+    (select-window cur-window)))
 
-;; -----------------------------
-;; Diagnostics
-;; -----------------------------
-(add-hook 'scheme-mode-hook #'flymake-mode)
+(defun guile--send (string)
+  "Low-level sender for Guile REPL."
+  (unless (comint-check-proc "*guile*")
+    (set-buffer (make-comint "guile" "/usr/bin/guile")))
+  (let ((proc (get-buffer-process "*guile*")))
+    (comint-send-string proc (concat string "\n"))))
 
+(defun guile-send-region (start end)
+  "Send region to Guile REPL."
+  (interactive "r")
+  (guile--send (buffer-substring-no-properties start end)))
 
-;; ===================================================================
-;; OLD BACKEND — CHICKEN (Comint Fallback) [COMMENTED]
-;; ===================================================================
+(defun guile-send-buffer ()
+  "Send whole buffer to Guile."
+  (interactive)
+  (guile-send-region (point-min) (point-max)))
 
-;; -----------------------------
-;; Geiser + CHICKEN integration
-;; -----------------------------
-;; (use-package geiser
-;;   :ensure t
-;;   :defer t
-;;   :config
-;;   (setq geiser-active-implementations '(chicken)))
+(defun guile-send-definition ()
+  "Send current definition to Guile."
+  (interactive)
+  (save-excursion
+    (mark-defun)
+    (guile-send-region (region-beginning) (region-end)))
+  (deactivate-mark))
 
-;; (use-package geiser-chicken
-;;   :ensure t
-;;   :after geiser
-;;   :config
-;;   (setq geiser-chicken-binary "/usr/bin/chicken-csi"))
+(defun guile-clear-repl ()
+  "Clear Guile REPL buffer."
+  (interactive)
+  (with-current-buffer "*guile*"
+    (let ((comint-buffer-maximum-size 0))
+      (comint-truncate-buffer))))
 
-;; -----------------------------
-;; CHICKEN Comint REPL
-;; -----------------------------
-
-;; (defun run-chicken ()
-;;   "Run CHICKEN Scheme REPL (csi) in a vertical split, keeping focus on the code buffer."
-;;   (interactive)
-;;   (unless (comint-check-proc "*chicken*")
-;;     (set-buffer (make-comint "chicken" "/usr/bin/chicken-csi")))
-;;   (let ((repl-buffer (get-buffer "*chicken*"))
-;;         (cur-window (selected-window)))
-;;     (unless (get-buffer-window repl-buffer)
-;;       (let ((new-window (split-window-right)))
-;;         (set-window-buffer new-window repl-buffer)))
-;;     (select-window cur-window)))
-
-;; (defun chicken--send-and-return (string)
-;;   "Send STRING to CHICKEN REPL."
-;;   (unless (comint-check-proc "*chicken*")
-;;     (set-buffer (make-comint "chicken" "/usr/bin/chicken-csi")))
-;;   (let ((proc (get-buffer-process "*chicken*")))
-;;     (comint-send-string proc (concat string "\n"))))
-
-;; (defun chicken-send-region (start end)
-;;   "Send region to CHICKEN."
-;;   (interactive "r")
-;;   (chicken--send-and-return (buffer-substring-no-properties start end)))
-
-;; (defun chicken-send-buffer ()
-;;   "Send buffer to CHICKEN."
-;;   (interactive)
-;;   (chicken-send-region (point-min) (point-max)))
-
-;; (defun chicken-clear-repl ()
-;;   "Clear REPL buffer."
-;;   (interactive)
-;;   (with-current-buffer "*chicken*"
-;;     (let ((comint-buffer-maximum-size 0))
-;;       (comint-truncate-buffer))))
-
-;; (with-eval-after-load 'scheme
-;;   (define-key scheme-mode-map (kbd "C-c C-c") #'chicken-send-definition)
-;;   (define-key scheme-mode-map (kbd "C-c C-r") #'chicken-send-region)
-;;   (define-key scheme-mode-map (kbd "C-c C-b") #'chicken-send-buffer)
-;;   (define-key scheme-mode-map (kbd "C-c C-z") #'run-chicken)
-;;   (define-key scheme-mode-map (kbd "C-c C-l") #'chicken-clear-repl))
+(with-eval-after-load 'scheme
+  (define-key scheme-mode-map (kbd "C-c C-c") #'guile-send-definition)
+  (define-key scheme-mode-map (kbd "C-c C-r") #'guile-send-region)
+  (define-key scheme-mode-map (kbd "C-c C-b") #'guile-send-buffer)
+  (define-key scheme-mode-map (kbd "C-c C-z") #'run-guile)
+  (define-key scheme-mode-map (kbd "C-c C-l") #'guile-clear-repl))
 
 
 (provide 'scheme-dev)

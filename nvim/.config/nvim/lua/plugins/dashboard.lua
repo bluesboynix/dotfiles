70bc09caf091988a71f20d596ca9b03183a3379b
bluesboy <bluesboychaosnix@gmail.com>
2026-02-04 23:59:18 +0530

    change theme to doom

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,29 +1,3 @@
--- return {
---   {
---     "nvimdev/dashboard-nvim",
---     event = "VimEnter",
---     dependencies = { "nvim-tree/nvim-web-devicons" },
---     config = function()
---       require("dashboard").setup({
---         theme = "hyper",
---         config = {
---             week_header = {
---             enable = true,
---           },
---           shortcut = {
---             { desc = "󰈞 Files", group = "@property", action = "Telescope find_files", key = "f" },
---             { desc = "󰊄 Recent", group = "@property", action = "Telescope oldfiles", key = "r" },
---             { desc = "󰏔 Config", group = "@property", action = "edit ~/.config/nvim/init.lua", key = "c" },
---             { desc = "󰈆 Quit", group = "@property", action = "qa", key = "q" },
---           },
---         },
---       })
---     end,
---   },
--- }
---
---
-
 return {
   {
     "nvimdev/dashboard-nvim",
@@ -31,42 +5,69 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("dashboard").setup({
-          theme = 'hyper',
-          config = {
-              week_header = {
-                enable = true,
-              },
-              shortcut = {
-                {
-                  desc = '󰊳 Update',
-                  group = '@property',
-                  action = 'Lazy update',
-                  key = 'u' },
-                {
-                  icon = ' ',
-                  icon_hl = '@variable',
-                  desc = 'Files',
-                  group = 'Label',
-                  action = 'Telescope find_files',
-                  key = 'f',
-                },
-                {
-                  desc = ' Apps',
-                  group = 'DiagnosticHint',
-                  action = 'Telescope app',
-                  key = 'a',
-                },
-                {
-                    desc = ' dotfiles',
-                    group = 'Number',
-                    action = 'Telescope dotfiles',
-                    key = 'd',
-                },
-              },
+        center = {
+          {
+            icon = '',
+            icon_hl = 'group',
+            desc = 'description',
+            desc_hl = 'group',
+            key = 'shortcut key in dashboard buffer not keymap !!',
+            key_hl = 'group',
+            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
+            action = '',
           },
+        },
+        footer = {},
+        vertical_center = false, -- Center the Dashboard on the vertical
       })
     end,
-  },
+  }
 }
 
 
+-- return {
+--   {
+--     "nvimdev/dashboard-nvim",
+--     event = "VimEnter",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     config = function()
+--       require("dashboard").setup({
+--           theme = 'hyper',
+--           config = {
+--               week_header = {
+--                 enable = true,
+--               },
+--               shortcut = {
+--                 {
+--                   desc = '󰊳 Update',
+--                   group = '@property',
+--                   action = 'Lazy update',
+--                   key = 'u' },
+--                 {
+--                   icon = ' ',
+--                   icon_hl = '@variable',
+--                   desc = 'Files',
+--                   group = 'Label',
+--                   action = 'Telescope find_files',
+--                   key = 'f',
+--                 },
+--                 {
+--                   desc = ' Apps',
+--                   group = 'DiagnosticHint',
+--                   action = 'Telescope app',
+--                   key = 'a',
+--                 },
+--                 {
+--                     desc = ' dotfiles',
+--                     group = 'Number',
+--                     action = 'Telescope dotfiles',
+--                     key = 'd',
+--                 },
+--               },
+--           },
+--       })
+--     end,
+--   },
+-- }
+--
+--

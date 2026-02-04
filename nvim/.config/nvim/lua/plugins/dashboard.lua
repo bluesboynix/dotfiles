a43ff85ccbf6cad1eae0403971caad8245b48de6
bluesboy <bluesboychaosnix@gmail.com>
2026-02-04 23:46:05 +0530

    update config

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,3 +1,29 @@
+-- return {
+--   {
+--     "nvimdev/dashboard-nvim",
+--     event = "VimEnter",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     config = function()
+--       require("dashboard").setup({
+--         theme = "hyper",
+--         config = {
+--             week_header = {
+--             enable = true,
+--           },
+--           shortcut = {
+--             { desc = "󰈞 Files", group = "@property", action = "Telescope find_files", key = "f" },
+--             { desc = "󰊄 Recent", group = "@property", action = "Telescope oldfiles", key = "r" },
+--             { desc = "󰏔 Config", group = "@property", action = "edit ~/.config/nvim/init.lua", key = "c" },
+--             { desc = "󰈆 Quit", group = "@property", action = "qa", key = "q" },
+--           },
+--         },
+--       })
+--     end,
+--   },
+-- }
+--
+--
+
 return {
   {
     "nvimdev/dashboard-nvim",
@@ -5,20 +31,42 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("dashboard").setup({
-        theme = "hyper",
-        config = {
-          week_header = {
-            enable = true,
+          theme = 'hyper',
+          config = {
+              week_header = {
+                enable = true,
+              },
+              shortcut = {
+                {
+                  desc = '󰊳 Update',
+                  group = '@property',
+                  action = 'Lazy update',
+                  key = 'u' },
+                {
+                  icon = ' ',
+                  icon_hl = '@variable',
+                  desc = 'Files',
+                  group = 'Label',
+                  action = 'Telescope find_files',
+                  key = 'f',
+                },
+                {
+                  desc = ' Apps',
+                  group = 'DiagnosticHint',
+                  action = 'Telescope app',
+                  key = 'a',
+                },
+                {
+                    desc = ' dotfiles',
+                    group = 'Number',
+                    action = 'Telescope dotfiles',
+                    key = 'd',
+                },
+              },
           },
-          shortcut = {
-            { desc = "󰈞 Files", group = "@property", action = "Telescope find_files", key = "f" },
-            { desc = "󰊄 Recent", group = "@property", action = "Telescope oldfiles", key = "r" },
-            { desc = "󰏔 Config", group = "@property", action = "edit ~/.config/nvim/init.lua", key = "c" },
-            { desc = "󰈆 Quit", group = "@property", action = "qa", key = "q" },
-          },
-        },
       })
     end,
   },
 }
+
 

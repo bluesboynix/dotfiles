2a145ef2c40f4e74a43663d0a6c20420ca7f72d8
bluesboynix <bluesboychaosnix@gmail.com>
2026-05-12 16:09:43 +0530

    change package.el to core-package.el due to recursive error

--- emacs/.config/emacs/lisp/core-package.el
+++ emacs/.config/emacs/lisp/core-package.el
@@ -1,18 +1,16 @@
-;;; package.el --- Minimal package management setup -*- lexical-binding: t; -*-
+;;; core-package.el --- Custom package setup -*- lexical-binding: t; -*-
 
-;; Set up package archives
-(require 'package)
+;; Do NOT (require 'package) here – it's already built-in
+
 (setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-;; Auto-refresh once a week
 (setq package-refresh-contents-if-time-critical t)
 
-;; If use-package isn't installed, offer to install it on first load
 (unless (package-installed-p 'use-package)
   (message "use-package not found. Run M-x package-refresh-contents, then M-x package-install use-package"))
 
-(provide 'package)
-;;; package.el ends here
+(provide 'core-package)
+;;; core-package.el ends here

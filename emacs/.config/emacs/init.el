1eaf450e55e853c7be37f3b83de7ed1f2ce3116d
bluesboynix <bluesboychaosnix@gmail.com>
2026-05-12 16:59:48 +0530

    add core-ui

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -13,10 +13,22 @@
 
 (require 'core-package)
 (require 'core-config)
-
+(require 'core-ui)
 
 
 
 (message "init.el loaded all modules successfully.")
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

93bb76661f46ee8c886aa790f6de9513ccd325bf
bluesboynix <bluesboychaosnix@gmail.com>
2026-05-12 18:12:37 +0530

    update init

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -17,3 +17,15 @@
 
 (message "init.el loaded all modules successfully.")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

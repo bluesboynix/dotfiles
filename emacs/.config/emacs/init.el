17414e948b8188595b18cf410806e2cd53febc0f
bluesboynix <bluesboychaosnix@gmail.com>
2026-05-12 17:59:06 +0530

    remove redundancy for reinstallation

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -17,16 +17,3 @@
 
 (message "init.el loaded all modules successfully.")
 
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(all-the-icons doom-modeline doom-themes fira-code-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

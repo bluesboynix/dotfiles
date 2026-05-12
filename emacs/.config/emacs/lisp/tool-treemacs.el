2b6e72f9167d1f7a4ba458ef1990f684a10b5b8a
bluesboynix <bluesboychaosnix@gmail.com>
2026-05-12 17:12:06 +0530

    add treemacs config

---
+++ emacs/.config/emacs/lisp/tool-treemacs.el
@@ -0,0 +1,46 @@
+;;; tool-treemacs.el --- Treemacs file explorer configuration -*- lexical-binding: t; -*-
+
+(require 'treemacs)
+
+;; ---------- Basic Setup ----------
+(setq treemacs-width 35
+      treemacs-indentation 2
+      treemacs-git-mode 'simple                     ; show git status
+      treemacs-file-event-delay 0.5)                ; debounce auto-refresh
+
+;; Remove mode line in treemacs buffer for cleaner look
+(add-hook 'treemacs-mode-hook
+          (lambda () (hide-mode-line-mode 1)))
+
+;; Auto-refresh when files change
+(treemacs-filewatch-mode t)
+
+;; ---------- Keybindings (global) ----------
+(global-set-key (kbd "C-x t t") 'treemacs)          ; toggle treemacs
+(global-set-key (kbd "C-x t C-t") 'treemacs-find-file) ; jump to current file in treemacs
+
+;; ---------- Integration with Projectile ----------
+(use-package treemacs-projectile
+  :ensure t
+  :after projectile
+  :config
+  (treemacs-projectile-default-setup))
+
+;; ---------- Icons in Dired (to match treemacs) ----------
+(use-package treemacs-icons-dired
+  :ensure t
+  :hook (dired-mode . treemacs-icons-dired-enable-once))
+
+;; ---------- Optional: Magit integration ----------
+(use-package treemacs-magit
+  :ensure t
+  :after magit)
+
+;; ---------- Optional: LSP integration (if you use lsp-mode) ----------
+;; (use-package lsp-treemacs
+;;   :ensure t
+;;   :after lsp-mode
+;;   :commands lsp-treemacs-errors-list)
+
+(provide 'tool-treemacs)
+;;; tool-treemacs.el ends here

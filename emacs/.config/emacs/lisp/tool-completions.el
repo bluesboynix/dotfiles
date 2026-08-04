15e54ebc35f0b700a33dac14079ddcdc91ec0c24
bluesboynix <bluesboychaosnix@gmail.com>
2026-08-04 17:36:31 +0530

    improve isearch

--- emacs/.config/emacs/lisp/tool-completions.el
+++ emacs/.config/emacs/lisp/tool-completions.el
@@ -21,7 +21,12 @@
   (vertico-mode)
   :custom
   (vertico-cycle t)
-  (vertico-count 10))
+  (vertico-count 10)
+  :bind (:map vertico-map
+              ("TAB" . minibuffer-complete)
+              ("<tab>" . minibuffer-complete)
+              ("S-TAB" . vertico-previous)
+              ("<backtab>" . vertico-previous)))
 
 ;;; Marginalia
 (use-package marginalia
@@ -43,7 +48,6 @@
   ;; popup appearance
   (corfu-count 10)
   (corfu-cycle t)
-  (corfu-count 8)
   (corfu-preview-current nil)
   (corfu-preselect 'prompt))
 
@@ -69,6 +73,28 @@
       completions-max-height 10
       resize-mini-windows 'grow-only
       max-mini-window-height 0.3)
+
+;;; Better isearch
+(setq isearch-lazy-count t          ; show match count (3/17)
+      lazy-count-prefix-format "[%s/%s] "
+      search-whitespace-regexp ".*?" ; spaces match any text
+      isearch-wrap-pause 'no
+      isearch-repeat-on-direction-change t)
+
+;; TAB completion from words in the current buffer
+(define-key isearch-mode-map (kbd "TAB") #'isearch-complete)
+(define-key isearch-mode-map (kbd "<tab>") #'isearch-complete)
+
+;; Better editing keys while searching
+(define-key isearch-mode-map (kbd "C-h") #'isearch-delete-char)
+(define-key isearch-mode-map (kbd "C-j") #'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "C-k") #'isearch-repeat-backward)
+
+;; Keep search string in history after exit
+(setq search-ring-max 50
+      regexp-search-ring-max 50)
+
+(setq search-default-mode #'char-fold-to-regexp)
 
 ;;; Faces
 (set-face-attribute 'minibuffer-prompt nil
